13250083d29779d5755279e25aabe3f942dde34e
MoreiraTv <matheusmoreira08@gmail.com>
2022-09-03 14:52:42 -0300

    add button

--- to-do-react/src/components/todo.js
+++ to-do-react/src/components/todo.js
@@ -1,18 +1,28 @@
 import ButtonTrash from "../imgs/buttonTrash";
 
+
+const handleClickTrash = (e) =>{
+  e.preventDefault();
+  console.log("Teste");
+}
+
 function ToDo(props) {
   return (
     <>
       {props.done !== true ? (
         <div className="to-do">
-          <ButtonTrash className="btn-trash" color={props.color}/>
+          <a className="btn-trash" onClick={handleClickTrash}>
+            <ButtonTrash color={props.color} className="btn-trash" />
+          </a>
           <label>{props.title}</label>
           <hr />
           <p>{props.descricao}</p>
         </div>
       ) : (
         <div className="to-do done">
-          <ButtonTrash className="btn-trash" color={props.color}/>
+          <a className="btn-trash" onClick={handleClickTrash}>
+            <ButtonTrash className="btn-trash" color={props.color} />
+          </a>
           <label>{props.title}</label>
           <hr />
           <p>{props.descricao}</p>
